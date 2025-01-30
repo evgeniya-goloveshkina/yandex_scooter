8d705d6fdda2c11e522c546164acd605e80f2504
Evgeniya Goloveshkina <evgeniya.goloveshkina@yandex.ru>
2025-01-30 17:45:40 +0100

    Commit sender_requests.py

--- sender_requests.py
+++ sender_requests.py
@@ -15,9 +15,6 @@ def post_new_order(order_body):
 
 response_post_new_order = post_new_order(data.order_body) # Вызов функции с телом запроса
 
-print("response_post_new_order: ", response_post_new_order.status_code)
-print("response_post_new_order.json(): ", response_post_new_order.json())  # Тело ответа в формате JSON
-
 # Отправка GET-запроса на получение заказа по его номеру /v1/orders/track?t=123456
 def get_order_by_track(track):
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + "?t=" + str(track),
